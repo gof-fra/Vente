6da3444800f1172e8064e7750433129238b749eb
hachim haggar <haggarhachim@gmail.com>
2018-07-16 02:58:31 +0100

    My project Setup Completed Successfully

---
+++ VenteBack/src/main/java/com/haggar/VenteBack/App.java
@@ -0,0 +1,13 @@
+package com.haggar.VenteBack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
