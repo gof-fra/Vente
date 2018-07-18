da35a4913b7cc966efb2a19e5014b884b8ec8016
hachim haggar <haggarhachim@gmail.com>
2018-07-18 02:37:15 +0100

    Completed the interface theme and css..

--- vente/src/main/java/com/haggar/vente/controller/PageController.java
+++ vente/src/main/java/com/haggar/vente/controller/PageController.java
@@ -12,31 +12,28 @@ public class PageController {
 	@RequestMapping(value = {"/", "/index", "/home"})
 	public ModelAndView index () {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcom to spring...");
-		
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
-	/*@RequestMapping(value="/test")
-	public ModelAndView test (@RequestParam(value="greeting", required=false) String greeting) {
-		if(greeting == null) {
-			greeting = "Hello there";
-		}
-			ModelAndView mv = new ModelAndView("page");
-			mv.addObject("greeting", greeting);
-			return mv;
-		
-	}*/
-	
-	@RequestMapping(value="/test/{greeting}")
-	public ModelAndView test (@PathVariable("greeting")String greeting) {
-		if(greeting == null) {
-			greeting = "Hello there";
-		}
-			ModelAndView mv = new ModelAndView("page");
-			mv.addObject("greeting", greeting);
-			return mv;
-		
+	@RequestMapping(value = "/about")
+	public ModelAndView about () {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
 	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact () {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
+	
+	
 
 }
